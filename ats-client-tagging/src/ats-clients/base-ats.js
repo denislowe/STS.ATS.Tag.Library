b7090e113350f48872e6dc0b8295223749d29e2b
Denny Riadi <denny.riadi@findly.com>
2017-04-27 15:57:24 +1200

    Implemented logic to handle apply start action for kenexa

--- ats-client-tagging/src/ats-clients/base-ats.js
+++ ats-client-tagging/src/ats-clients/base-ats.js
@@ -4,6 +4,11 @@ function BaseAts(collectorUrl, collectorDefaultQueryParams, params) {
   this.collectorUrl = collectorUrl;
   this.collectorDefaultQueryParams = collectorDefaultQueryParams;
   this.params = params;
+
+  this.knownActions = {
+    appStart: 'App_Start',
+    appComplete: 'App_Complete'
+  };
 }
 
 BaseAts.prototype.getAppId = function() {
@@ -18,11 +23,29 @@ BaseAts.prototype.getApplicantId = function() {
   return this.params.applicantid;
 };
 
+BaseAts.prototype.getAction = function() {
+  var action = this.params.action;
+
+  if (!action) {
+    return;
+  }
+
+  action = action.toLowerCase();
+  if (action === 'app_start' || action.indexOf('start') >= 0) {
+    return this.knownActions.appStart;
+  }
+
+  if (action === 'app_complete' || action.indexOf('complete') >= 0) {
+    return this.knownActions.appComplete;
+  }
+};
+
 BaseAts.prototype.generateQueryParameters = function() {
   var result = '',
     appId = this.getAppId(),
     applicantId = this.getApplicantId(),
-    reqId = this.getReqId();
+    reqId = this.getReqId(),
+    action = this.getAction();
 
   if (appId) {
     result += '&aid=' + encodeURIComponent(appId);
@@ -34,6 +57,10 @@ BaseAts.prototype.generateQueryParameters = function() {
 
   if (reqId) {
     result += '&se_la=' + encodeURIComponent(reqId);
+  }
+
+  if (action) {
+    result += '&se_ac=' + encodeURIComponent(action);
   }
 
   // Force browser to hit the URL instead of retrieving the content from cache?
--- ats-client-tagging/src/ats-clients/kenexa.js
+++ ats-client-tagging/src/ats-clients/kenexa.js
@@ -5,11 +5,52 @@ var util = require('util'),
 
 function Kenexa(collectorUrl, collectorDefaultQueryParams, params) {
   BaseAts.call(this, collectorUrl, collectorDefaultQueryParams, params);
+
+  if (this.getAction() === this.knownActions.appStart) {
+    this._storeReqId(this._reqIdOnAppStart());
+  }
 }
 
 util.inherits(Kenexa, BaseAts);
 
-Kenexa.prototype.getReqId = function() {
+Kenexa.prototype._storeReqId = function(reqId) {
+  if (!reqId) {
+    return;
+  }
+
+  window.sessionStorage.setItem('stcJobId', reqId);
+};
+
+Kenexa.prototype._reqIdOnAppStart = function() {
+  var buttons = document.getElementsByTagName('a'),
+    match;
+
+  if (buttons) {
+    for (var i = 0; i < buttons.length; i++) {
+      if (buttons[i].className &&
+        buttons[i].className.indexOf('job-apply') > -1 &&
+        buttons[i].href &&
+        /sjobs\.brassring\.com/i.test(buttons[i].href) &&
+        buttons[i].href.match(/jobid=(\d+)/i)
+      ) {
+        return buttons[i].href.match(/jobid=(\d+)/i)[1];
+      }
+    }
+  }
+
+  if (window.location.href.match(/partnerid=25782/i) &&
+    window.location.href.match(/siteid=5121/i) &&
+    window.document.getElementById('Auto req ID')) {
+    return window.document.getElementById('Auto req ID').innerHTML;
+  }
+
+  match = window.location.href.match(/jobid=(\w+)/i) || window.location.href.match(/areq=(\w+)/i);
+  if (match) {
+    return match[1];
+  }
+};
+
+Kenexa.prototype._reqIdOnAppComplete = function() {
   var elements = window.document.getElementsByName('JobInfo'),
     jobId = null,
     jobIdNewBr = window.document.getElementById('hSMJobId');
@@ -30,8 +71,17 @@ Kenexa.prototype.getReqId = function() {
     return jobIdNewBr.value;
   }
 
-  if (window.sessionStorage && window.sessionStorage.getItem('jobId')) {
-    return sessionStorage.getItem('jobId');
+  if (window.sessionStorage && window.sessionStorage.getItem('stcJobId')) {
+    return sessionStorage.getItem('stcJobId');
+  }
+};
+
+Kenexa.prototype.getReqId = function() {
+  switch (this.getAction()) {
+    case this.knownActions.appStart:
+      return this._reqIdOnAppStart();
+    default:
+      return this._reqIdOnAppComplete();
   }
 };
 
