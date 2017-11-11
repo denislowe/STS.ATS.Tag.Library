999050bb28f3aff430320b02945d37037b4699b8
Denny Riadi <denny.riadi@findly.com>
2017-11-11 23:24:48 +1300

    Tag library for peoplesoft

---
+++ ats-client-tagging/src/ats-clients/peoplefluent.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var util = require('util'),
+  BaseAts = require('./base-ats');
+
+function PeopleSoft(collectorUrl, collectorDefaultQueryParams, params) {
+  BaseAts.call(this, collectorUrl, collectorDefaultQueryParams, params);
+}
+
+util.inherits(PeopleSoft, BaseAts);
+
+PeopleSoft.prototype.getReqId = function() {
+  var spans = document.getElementsByTagName('span');
+
+  for (var i = 0; i < spans.length; i++) {
+    if (spans[i].className !== 'pagesubheading') {
+      break;
+    }
+
+    var match = spans[i].innerHTML.trim().match(/(\d+)$/);
+    if (match) {
+      return match[1];
+    }
+  }
+
+  return '';
+};
+
+module.exports = PeopleSoft;
